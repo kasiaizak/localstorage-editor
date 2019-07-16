742c477f25aeaa974afe62d0b58fa395d8943eed
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-07-16 09:55:03 +0200

    fix arrow function

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ const editorArea = document.querySelector('.editor__area--js');
 const saveButton = document.querySelector('.editor__button--save-js');
 const loadButton = document.querySelector('.editor__button--load-js');
 
-saveButton.addEventListener('click' => {
+saveButton.addEventListener('click', () => {
   localStorage.setItem('textinput' , editorArea.value);
 });
 
-loadButton.addEventListener('click' => {
+loadButton.addEventListener('click', () => {
   editorArea.value = localStorage.getItem('textinput');
 });
