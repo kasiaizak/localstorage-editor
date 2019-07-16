dc972a23ac670a5b57f50fbcca6822eb81656cc5
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-07-16 08:49:22 +0200

    del (a) parametr

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ const editorArea = document.querySelector('.editor__area--js');
 const saveButton = document.querySelector('.editor__button--save-js');
 const loadButton = document.querySelector('.editor__button--load-js');
 
-saveButton.addEventListener('click', (e) => {
-  localStorage.setItem('textinput',editorArea.value);
+saveButton.addEventListener('click' => {
+  localStorage.setItem('textinput' , editorArea.value);
 });
 
-loadButton.addEventListener('click', (e) => {
+loadButton.addEventListener('click' => {
   editorArea.value = localStorage.getItem('textinput');
 });
