4be52dc9db4c10d41415ceb603c0eee7416501be
Kasia Izak <kontakt@kasiaizak.pl>
2019-10-07 11:12:53 +0200

    add browser-update package

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 "use strict";
 
+import browserUpdate from 'browser-update';
+browserUpdate({
+  required: {e:11,f:-6,o:-6,s:-2,c:-6},
+  insecure:true,
+  unsupported:true,
+  reminder:0,
+  reminderClosed: 1
+});
+
 const editorArea = document.querySelector('.editor__area--js');
 const saveButton = document.querySelector('.editor__button--save-js');
 const loadButton = document.querySelector('.editor__button--load-js');
